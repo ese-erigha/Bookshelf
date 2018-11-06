2876516ec10ecd9cdb2ea010bc548998a07671a1
Eseoghene Erigha <ese.erigha@gmail.com>
2018-11-06 11:25:26 +0100

    Added files
    Added nodemon,mongoose,typescript,morgan,winston

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+var morgan = require('morgan');
+var winston = require('./config/winston.js');
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+//import TYPES from './types';
+//import container from './inversify.config';
+//import {RegistrableController} from './controller/RegisterableController';
+
+// create express application
+const app: express.Application = express();
+// let express support JSON bodies
+app.use(bodyParser.json());
+app.use(morgan('combined', { stream: winston.stream }));
+
+// grabs the Controller from IoC container and registers all the endpoints
+//const controllers: RegistrableController[] = container.getAll<RegistrableController>(TYPES.Controller);
+//controllers.forEach(controller => controller.register(app));
+
+// setup express middleware logging and error handling
+app.use(function (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
+    
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // add this line to include winston logging
+    winston.error(`${err.stack || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+    next(err);
+});
+
+app.use(function (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
+    res.status(500).send('Internal Server Error');
+});
+
+app.listen(3000, function () {
+    winston.info('Example app listening on port 3000!');
+});
